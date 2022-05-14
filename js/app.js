40d3cd3faf3f9137193497e980758789b05c3004
uttamsaha <uttamsaha982@gmail.com>
2022-05-14 12:22:19 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-document.getElementById('black').addEventListener('click',()=>{
-   const img =  document.getElementById('img').src = "../images/BMW3.png";
+document.getElementById("black").addEventListener("click", () => {
+  document.getElementById("img").src = "../images/BMW3.png";
 });
-document.getElementById('red').addEventListener('click',()=>{
-   const img =  document.getElementById('img').src = "../images/BMW1.png";
+document.getElementById("red").addEventListener("click", () => {
+  document.getElementById("img").src = "../images/BMW1.png";
 });
-document.getElementById('blue').addEventListener('click',()=>{
-   const img =  document.getElementById('img').src = "../images/BMW2.png";
+document.getElementById("blue").addEventListener("click", () => {
+  document.getElementById("img").src = "../images/BMW2.png";
 });
