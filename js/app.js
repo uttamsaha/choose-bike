27c155c1fb9576bcb6298c1140a75b8cfd0422da
uttamsaha <uttamsaha982@gmail.com>
2022-05-31 10:15:15 +0600

    img source path changed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 document.getElementById("black").addEventListener("click", () => {
-  document.getElementById("img").src = "../images/BMW3.png";
+  document.getElementById("img").src = "https://i.ibb.co/PxV4XpM/BMW3.png";
 });
 document.getElementById("red").addEventListener("click", () => {
-  document.getElementById("img").src = "../images/BMW1.png";
+  document.getElementById("img").src = "https://i.ibb.co/ZHVDDZ9/BMW1.png";
 });
 document.getElementById("blue").addEventListener("click", () => {
-  document.getElementById("img").src = "../images/BMW2.png";
+  document.getElementById("img").src = "https://i.ibb.co/wNpxJXh/BMW2.png";
 });
